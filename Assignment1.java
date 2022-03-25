88903515f5042bee8ff05e55608b7606a17da43e
babarjawad10 <102314811+babarjawad10@users.noreply.github.com>
2022-03-25 11:49:36 +0500

    code readability improve and out layout improve

--- Assignment1.java
+++ Assignment1.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 /**
  * Write a description of class Assignment1 here.
  *
- * @author (your name)
+ * @author (Babar jawad)
  * @version (a version number or a date)
  */
 public class Assignment1
 {
-    // instance variables - replace the example below with your own
-    private String unitName; // 
-    private int[] studentMarks;
-    private int temp;
-    private boolean check;
+    // instance variables - 
+    private String unitName; // declare variable for storing value for unit name 
+    private int[] studentMarks; //array to store students marks 
+    private int temp;      //for store temporary values  
+    private boolean check; // for conditaion validation 
     
 
     /**
@@ -20,12 +20,16 @@ public class Assignment1
     public Assignment1()
     {
         // initialise instance variables
-        studentMarks=new int[25];
-        check=true;
+        studentMarks=new int[25]; // dynamically alocating the memory to array
+        check=true; // store the value true
         
     }// endof constructor
     
     
+    /**
+     * Method getUnitName
+     * take the input from user and store the input in unitName 
+     */
     public void getUnitName(){
         System.out.println("Please Enter unit name");
         unitName = new Scanner(System.in).nextLine();
@@ -33,12 +37,18 @@ public class Assignment1
     
     
     
+    /**
+     * Method getStudentMarks
+     * take input of 25 students from user and store into studentMarks array
+     * also only take input from 0 to 100 interger number only 
+     */
     public void getStudentMarks(){
         System.out.println("Please enter 25 Student marks between 0-100");
         for(int i=0;i<25;i++){
+            
             while(check){
             try{
-            temp=new Scanner(System.in).nextInt();
+            temp=new Scanner(System.in).nextInt();   
             }// end of try
             catch(Exception e){
             System.out.println("Invalid input");
@@ -61,6 +71,10 @@ public class Assignment1
         
     }// end of getStudentMarks
     
+    /**
+     * Method findHighestLowestMarks
+     * find the highest and lowest marks using linear search
+     */
     public void findHighestLowestMarks(){
         
         int highestMark = studentMarks[0];
@@ -74,13 +88,20 @@ public class Assignment1
               lowestMark=studentMarks[i];
             }
     }//end of for loop
-    
+    System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
     System.out.println("The highest mark is:"+ highestMark);
+    System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
     System.out.println("The lowest mark is:"+lowestMark);
+    System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
         
 }// endof findHighestLowestMarks
 
 
+/**
+ * Method findMeanAndStandardDeviationOfStudnetMarks
+ * calculate mean and standard deviation of studentMarks array
+ *
+ */
 public void findMeanAndStandardDeviationOfStudnetMarks(){
     double sum=0,mean;
     
@@ -91,6 +112,7 @@ public void findMeanAndStandardDeviationOfStudnetMarks(){
     mean=sum/studentMarks.length;
     
            System.out.println("Mean of student Marks is :"+mean);
+           System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
            
            
         sum=0;  
@@ -103,26 +125,38 @@ public void findMeanAndStandardDeviationOfStudnetMarks(){
     double standradDeviation=Math.sqrt(mean);
     System.out.format("Standard deviation of student marks : %.6f",standradDeviation);
     
-    
+   
     
 }//endof findMeanAndStandardDeviationOfStudnetMarks
 
 
     
+    /**
+     * Method printUnitName
+     * display unit name 
+     *
+     */
     public void printUnitName(){
         System.out.println("The Unit Name is:  "+unitName);
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
     }
     
+    /**
+     * Method printStudentMarks
+     * display valid students marks 
+     *
+     */
     public void printStudentMarks(){
         System.out.println("Student marks you entered are: ");
         for(int i=0;i<25;i++){
             System.out.println(studentMarks[i]);
         }
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
         
     }
         
     public static void main(String[] args){
-        Assignment1 myObj=new Assignment1();
+        Assignment1 myObj=new Assignment1(); // create object of Assignment class 
         
         myObj.getUnitName();
         myObj.getStudentMarks();
